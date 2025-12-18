c3f65a3792058fb38bcd0396a274981c99ef56ff
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2025-12-18 00:58:26 -0800

    feat: add operate function
    Created a new function operate that takes an operator and two numbers
    then calls one of the basic math operation functions on the numbers.

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const divide = (a, b) => b === 0 ? "Error" : a / b;
 let firstNumber;
 let operator;
 let secondNumber;
+
+const operate = (operator, a, b) => {
+  if (operator === "+") return add(a, b);
+  if (operator === "-") return subtract(a, b);
+  if (operator === "*") return multiply(a, b);
+  if (operator === "/") return divide(a, b);
+};
